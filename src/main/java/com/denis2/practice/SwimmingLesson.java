51cff0c4c92ebd2ed2a80cfd7e1c993cb86f5dec
DenisTikholaz <155545713+DenisTikholaz@users.noreply.github.com>
2024-12-12 04:14:53 +0200

    Revert "Day 3"

--- src/main/java/com/denis2/practice/SwimmingLesson.java
+++ src/main/java/com/denis2/practice/SwimmingLesson.java
@@ -1,23 +1,11 @@
 package com.denis2.practice;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "swimming_lesson")
 public class SwimmingLesson {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String groupName;
     private int lessonNumber;
     private String date;
     private String coach;
     private int poolNumber;
-
-    public SwimmingLesson() {
-    }
 
     public SwimmingLesson(String groupName, int lessonNumber, String date, String coach, int poolNumber) {
         this.groupName = groupName;
@@ -25,14 +13,7 @@ public class SwimmingLesson {
         this.date = date;
         this.coach = coach;
         this.poolNumber = poolNumber;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getGroupName() {
--- src/main/resources/db/migration/V1__create_schedule_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE swimming_lesson (
-    id BIGSERIAL PRIMARY KEY,
-    group_name VARCHAR(255) NOT NULL,
-    lesson_number INTEGER NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    coach VARCHAR(255) NOT NULL,
-    pool_number INTEGER NOT NULL
-);
-
